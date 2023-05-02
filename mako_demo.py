19c5d985d0db4639911a56c887fdd89cc4b11baf
Kris <kristian_dilov@abv.bg>
2023-05-02 22:09:16 +0100

    task1 working

--- mako_demo.py
+++ mako_demo.py
@@ -1,15 +1,21 @@
 from mako.template import Template
 
-mytemplate = Template("hello ${name}")
 
-print(mytemplate.render(name="josh"))
-
-#TASK 1: Create an HTML table from a list of dictionaries in Python. 
-# Each dictionary represents a row in the table, and the keys 
-# represent the column names. The table should have alternating 
+# TASK 1: Create an HTML table from a list of dictionaries in Python.
+# Each dictionary represents a row in the table, and the keys
+# represent the column names. The table should have alternating
 # row colors and should include a header row.
 
 list_of_cols = ["dog_name", "age", "height"]
 
-list_of_dicts = [{"dog_name": "vsv", "age": 12, "height": 3}, {"dog_name": "ger", "age": 5, "height": 7}]
+list_of_dicts = [
+    {"dog_name": "vsv", "age": 12, "height": 3},
+    {"dog_name": "ger", "age": 5, "height": 7},
+]
 
+mytemplate = Template(filename="table.html")
+
+result = mytemplate.render(list_of_cols=list_of_cols, list_of_dicts=list_of_dicts)
+
+with open("output.html", "w") as f:
+    f.write(result)

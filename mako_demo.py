f4ff7fe3a89f51becfd0d81afc08c966a6865319
Kris <kristian_dilov@abv.bg>
2023-05-02 23:28:27 +0100

    task2 done

--- mako_demo.py
+++ mako_demo.py
@@ -1,5 +1,5 @@
 from mako.template import Template
-
+from dataclasses import dataclass
 
 # TASK 1: Create an HTML table from a list of dictionaries in Python.
 # Each dictionary represents a row in the table, and the keys
@@ -19,3 +19,27 @@ result = mytemplate.render(list_of_cols=list_of_cols, list_of_dicts=list_of_dict
 
 with open("output.html", "w") as f:
     f.write(result)
+
+
+# TASK 2: Task: Create an XML document that represents
+# a list of books, each with a title, author, and publication year.
+
+
+@dataclass
+class Books:
+    title: str
+    author: str
+    year: int
+
+
+mytemplate2 = Template(filename="books.xml")
+
+data = [
+    Books("Gog", "ASD", 1234),
+    Books("BNSD", "grh", 234),
+    Books("VZRW", "twr", 7546),
+]
+
+result = mytemplate2.render(books=data)
+
+print(result)

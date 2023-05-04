e69986f273f22f0cc7a609b47d2143c34722d666
Kris <kristian_dilov@abv.bg>
2023-05-04 23:16:06 +0100

    task 4 complete

--- lxml_practise.py
+++ lxml_practise.py
@@ -1,2 +1,16 @@
-import lxml
+from lxml import etree
+from mako.template import Template
+# TASK 4
+doc = etree.parse("movies.xml")
 
+movies = doc.findall("movie")
+print(movies)
+
+list_of_cols = ["title", "director", "year", "rating"]
+
+mytemplate = Template(filename="movie_table.html")
+
+res = mytemplate.render(list_of_cols=list_of_cols, movies=movies)
+
+with open("output2.html", "w") as f:
+    f.write(res)

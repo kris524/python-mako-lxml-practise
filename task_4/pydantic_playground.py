538c466efd1171917fbd831566c4c48393d39737
Kris <kristian_dilov@abv.bg>
2023-05-07 23:43:57 +0100

    reorganize

--- task_4/pydantic_playground.py
+++ task_4/pydantic_playground.py
@@ -4,16 +4,29 @@
 # validates and processes the data using Pydantic, 
 # and prints the valid users and their details in a formatted output.
 import json
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Optional
 json_file = open("users.json")
 
 data = json.load(json_file)
 print(data)
 
-class Users(BaseModel):
+class User(BaseModel):
     name: str
     email: str
     age: int
     city: Optional[str]
-    
+
+    @validator("name")
+    @classmethod
+    def validate_name(cls, name):
+        pass
+
+
+# for entry in data:
+
+#     try:
+
+#     a = User(name=entry["name"],email=entry["email"], age=entry["age"], city=entry["city"] )
+
+

f27f1ab6cf4d9421dcf4fc6956c4e03af37757e3
Kris <kristian_dilov@abv.bg>
2023-05-08 11:36:58 +0100

    working pydantic validator

--- task_4/pydantic_playground.py
+++ task_4/pydantic_playground.py
@@ -1,15 +1,16 @@
 # Pydantic task
-# Task: Create a Python script 
-# that takes a JSON file with user data, 
-# validates and processes the data using Pydantic, 
+# Task: Create a Python script
+# that takes a JSON file with user data,
+# validates and processes the data using Pydantic,
 # and prints the valid users and their details in a formatted output.
 import json
 from pydantic import BaseModel, validator
 from typing import Optional
+
 json_file = open("users.json")
 
 data = json.load(json_file)
-print(data)
+
 
 class User(BaseModel):
     name: str
@@ -20,13 +21,32 @@ class User(BaseModel):
     @validator("name")
     @classmethod
     def validate_name(cls, name):
-        pass
+        if name.islower():
+            raise ValueError(f"The name needs to start with a capital letter, name given: {name}")
+        else:
+            return name
+
+    @validator("age")
+    @classmethod
+    def validate_age(cls, age):
+        if age <= 0:
+            raise ValueError(f"The age needs to be higher than 0, age given: {age}")
+        else:
+            return age
 
 
-# for entry in data:
+def validator(data):
+    for entry in data:
+        
+        User(
+            name=entry["name"],
+            email=entry["email"],
+            age=entry["age"],
+            city=entry["city"],
+        )
 
-#     try:
-
-#     a = User(name=entry["name"],email=entry["email"], age=entry["age"], city=entry["city"] )
+    print("EVERYTHING IS VALID")
 
 
+if __name__ == "__main__":
+    validator(data)

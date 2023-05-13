5f5ae1e6da0a1def562825c41d3a218fc35b9012
Kris <kristian_dilov@abv.bg>
2023-05-13 16:09:41 +0100

    experiment2

---
+++ random_experiments/experiment2.py
@@ -0,0 +1,54 @@
+from typing import Protocol
+import math
+
+# In this exercise, you'll
+# implement two classes representing geometric
+# shapes, a Circle and a Rectangle, that both conform to a
+#  Shape protocol. You'll also create a function that
+# accepts any object conforming to the Shape protocol
+#  and calculates its perimeter.
+
+
+class Shape(Protocol):
+    def area(self) -> float:
+        pass
+
+    def perimiter(self)-> float:
+        pass
+
+
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def area(self) -> int:
+        return self.a * self.b
+
+    def perimiter(self) -> float:
+        return 2 *  (self.a + self.b)
+
+
+class Circle:
+    def __init__(self, a):
+        self.a = a
+
+    def area(self) -> int:
+        return (self.a**2) * math.pi
+
+    def perimiter(self) -> float:
+        return 2 * math.pi * self.a
+
+
+def calculate_area(shape: Shape) -> float:
+    return shape.area()
+
+def calculate_peremiter(shape: Shape) -> float:
+    return shape.perimiter()
+
+if __name__ == "__main__":
+    cir = Circle(2)
+    print(calculate_area(cir))
+
+    rec = Rectangle(1,2)
+    print(calculate_peremiter(rec))

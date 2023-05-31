ff28c5435d9fa79d62c0c2f6fbb47161d140c121
Kris <kristian_dilov@abv.bg>
2023-05-31 22:17:11 +0100

    task 5

---
+++ task_5/practise_with_ipxact.py
@@ -0,0 +1,5 @@
+# Question: Extract and Print the Component Details from the IP-XACT file
+
+# Write a Python script to extract and print the vendor, library, 
+# name, and version details of the component described in the CHI-D-RND 
+# IP-XACT file. For this, you will need to parse the XML file and locate the relevant information.

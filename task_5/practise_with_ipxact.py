2012827c2b684dfda4004233ed2f1b2388862654
Kris <kristian_dilov@abv.bg>
2023-06-01 00:10:53 +0100

    one way

--- task_5/practise_with_ipxact.py
+++ task_5/practise_with_ipxact.py
@@ -3,3 +3,30 @@
 # Write a Python script to extract and print the vendor, library, 
 # name, and version details of the component described in the CHI-D-RND 
 # IP-XACT file. For this, you will need to parse the XML file and locate the relevant information.
+import lxml.etree as etree
+
+
+doc = etree.parse("CHI_D_RND_rtl.xml")
+
+ns = {"spirit": "http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009"}
+
+
+a = doc.xpath("//*[@library]")
+
+via_attributes = doc.xpath("//spirit:busType[@library]", namespaces=ns)
+
+
+
+direct_access_vendor = doc.xpath("//spirit:vendor/text()", namespaces=ns)
+direct_access_library = doc.xpath("//spirit:library/text()", namespaces=ns)
+direct_access_version = doc.xpath("//spirit:version/text()", namespaces=ns)
+direct_access_name = doc.xpath("//spirit:name/text()", namespaces=ns)
+
+
+if __name__ == "__main__":
+    print(via_attributes)
+
+    print(direct_access_vendor)
+    print(direct_access_library)
+    print(direct_access_version)
+    print(direct_access_name)

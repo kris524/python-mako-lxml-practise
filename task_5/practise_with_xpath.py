695f6b631ffce5bc1e260501221bba167b4747da
Kris <kristian_dilov@abv.bg>
2023-06-01 10:55:17 +0100

    xpath

--- task_5/practise_with_xpath.py
+++ task_5/practise_with_xpath.py
@@ -13,20 +13,24 @@ ns = {"spirit": "http://www.spiritconsortium.org/XMLSchema/SPIRIT/1685-2009"}
 
 a = doc.xpath("//*[@library]")
 
-via_attributes = doc.xpath("//spirit:busType[@library]", namespaces=ns)
+via_attributes = doc.xpath("//spirit:busType[@spirit:library]", namespaces=ns)
 
 
-
+#TODO: Use a for loop to iterate over the elements, makes it look better
 direct_access_vendor = doc.xpath("//spirit:vendor/text()", namespaces=ns)
 direct_access_library = doc.xpath("//spirit:library/text()", namespaces=ns)
 direct_access_version = doc.xpath("//spirit:version/text()", namespaces=ns)
 direct_access_name = doc.xpath("//spirit:name/text()", namespaces=ns)
 
 
+logicalName = doc.xpath("//spirit:logicalName", namespaces=ns)
+
 if __name__ == "__main__":
-    print(via_attributes)
+    print(via_attributes[0])
 
     print(direct_access_vendor)
     print(direct_access_library)
     print(direct_access_version)
     print(direct_access_name)
+
+    print(logicalName[0].text)
